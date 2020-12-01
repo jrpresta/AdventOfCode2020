89cdd40dcb36a00646804ca951a0d645bb9aa520
jrpresta <jonrosspresta@gmail.com>
2020-12-01 12:25:49 -0800

    Day 1 complete

---
+++ Day1/puzzle.py
@@ -0,0 +1,21 @@
+def puzzle_1(file_path):
+    with open(file_path, 'r') as f:
+        values = [int(v.strip()) for v in f.readlines()]
+
+    for i in values:
+        for j in values:
+            if i + j == 2020: return i*j
+
+def puzzle_2(file_path):
+    with open(file_path, 'r') as f:
+        values = [int(v.strip()) for v in f.readlines()]
+
+    for i in values:
+        for j in values:
+            for k in values:
+                if i + j + k == 2020: return i*j*k
+
+
+if __name__ == '__main__':
+    print(puzzle_1('./input.csv'))
+    print(puzzle_2('./input.csv'))
